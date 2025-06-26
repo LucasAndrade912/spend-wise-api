97c4e189e75ebd3c0ab45a043f56d6634f8b3c51
Lucas Andrade <lucasantos.dev@gmail.com>
2025-06-26 00:37:06 -0300

    fix: update server to use dynamic port from environment variable

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import { app } from './app';
 
 const start = async () => {
+    const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
     try {
-        await app.listen({ port: 3333 });
+        await app.listen({ port, host: '0.0.0.0' });
     } catch (err) {
         app.log.error(err);
         process.exit(1);
